4f5f835c6a9bdf91fecfd7dd5142b14deb0b5e39
sunxiangyu <sunxiangyu@huli.com>
2019-03-28 16:00:43 +0800

    springmvc配置

--- src/main/src/java/com/sxy/www/controller/TestController.java
+++ src/main/src/java/com/sxy/www/controller/TestController.java
@@ -4,16 +4,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by xiangyusun on 2019/3/28.
  */
 @Controller
+@RequestMapping("demo")
 public class TestController {
 
     Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping(value = "healthCheck")
+    @ResponseBody
     public String healthCheck(){
 
         logger.info("healthCheck");
