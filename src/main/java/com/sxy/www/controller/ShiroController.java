27757741f8c7e2a9960d52ddcda415c7d920f7fd
sunxiangyu <sunxiangyu@huli.com>
2019-10-19 12:06:43 +0800

    集成shiro

---
+++ src/main/java/com/sxy/www/controller/ShiroController.java
@@ -0,0 +1,12 @@
+package com.sxy.www.controller;
+
+/**
+ * @ClassName ShiroController
+ * @Description TODO
+ * @Author sunxiangyu
+ * @Email sunxiangyu@huli.com
+ * @Date 2019-10-18 17:57
+ * @Version 1.0
+ **/
+public class ShiroController {
+}
---
+++ src/main/java/com/sxy/www/shiro/MyRealm.java
@@ -0,0 +1,12 @@
+package com.sxy.www.shiro;
+
+/**
+ * @ClassName MyRealm
+ * @Description TODO
+ * @Author sunxiangyu
+ * @Email sunxiangyu@huli.com
+ * @Date 2019-10-18 17:55
+ * @Version 1.0
+ **/
+public class MyRealm {
+}
